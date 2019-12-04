3ebf911738b9b683d39b52cf111093d0854cddd0
abudiab <mahmoud.w.diab@gmail.com>
2019-12-04 10:47:24 +0200

    Installed Jest+Enzyme and did some tut with semantic on signin form

--- wenak app/src/App.tsx
+++ wenak app/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './app.scss';
+import LoginForm from './components/loginForm';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      
+      <h1>App</h1>
+
     </div>
   );
 }
