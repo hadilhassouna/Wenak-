157ba7d56211f8190e998784f21ab5915c1ccbda
unknown <hadil.a.hassouna@gmail.com>
2019-12-05 13:41:22 -0800

    nav bar

--- wenak app/src/App.tsx
+++ wenak app/src/App.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
-
+import Toolbar from './components/Toolbar/Toolbar.jsx';
+import SideDrawer from './components/SideDrawer/SideDrawer.jsx';
+import Backdrop from './components/Backdrop/Backdrop.jsx'
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to space reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height: '100%'}}>
+      <Toolbar />
+      <SideDrawer />
+      <Backdrop />
+      <main style={{marginTop: '64px'}}>
+      <p>This is the content of the page</p>
+      </main>
+      
     </div>
   );
 }
