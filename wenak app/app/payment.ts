f2819e659a43ce5bece55599911afe69325d9129
unknown <hadil.a.hassouna@gmail.com>
2019-12-15 08:00:16 -0800

    TRAVIC CI

--- wenak app/app/payment.ts
+++ wenak app/app/payment.ts
@@ -1,24 +1,22 @@
-const express = require('express');
-const router= express.Router();
-const bodyParser = require('body-parser');
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({extended: false}));
 // router.use(bodyParser.json());
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);//?
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY); // ?
 router.use(require("body-parser").text());
-
-
 
 router.post("/charge", async (req: any, res: any) => {
   try {
-    let {status} = await stripe.charges.create({
-      amount: 300, //cents
+    const {status} = await stripe.charges.create({
+      amount: 300, // cents
       currency: "usd",
       description: "Delivery fee",
       source: req.body
     });
 
-    res.json({status, Success:'Success'});
+    res.json({status, Success: "Success"});
   } catch (err) {
     console.log(err);
     res.status(500).end();
@@ -27,5 +25,5 @@ router.post("/charge", async (req: any, res: any) => {
 
 // app.listen(9000, () => console.log("Listening on port 9000"));
 
-export {}
+export {};
 module.exports = router;
