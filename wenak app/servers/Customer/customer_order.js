606bf9c18b7bf00067d539f5b8c077843bb9e3a8
amalabed <amalaljarousha93@gmail.com>
2019-12-17 16:04:39 +0200

    last version

--- wenak app/servers/Customer/customer_order.js
+++ wenak app/servers/Customer/customer_order.js
@@ -14,47 +14,50 @@ const ObjectId = require('mongodb').ObjectID;
 var VerifyToken = require('../authontication/AuthController.js');
 
 ///send order.
-router.post("/send_order",VerifyToken ,function(req, res, next) {
+router.post("/send_order", VerifyToken, function (req, res, next) {
     //var myId = JSON.parse(req.userId);
     User.findById((req.userId), { password: 0 }, function (err, user) {
-        if (err) return res.status(500).send("There was a problem finding the user.");
-        if (!user) return res.status(404).send("No user found.");
+        if (err)
+            return res.status(500).send("There was a problem finding the user.");
+        if (!user)
+            return res.status(404).send("No user found.");
         console.log(req);
-
-   //var id = ObjectId(JSON.parse(req.userId));
-    // console.log(id);
-    const myorder = req.body;
-    console.log(myorder);
-    if (!myorder)
-        return res.status(400).json({ msg: "problem in order" });
-    else if (myorder) {
-        Order.create({
-            user_id: user._id,
-            driver_id:"0",
-            order_details: myorder.order_details,
-            location_start_lng: myorder.location_start_lng,
-            location_start_lat: myorder.location_start_lat,
-            location_end_lng: myorder.location_end_lng,
-            location_end_lat: myorder.location_end_lat,
-            reciver_name: myorder.reciver_name,
-            recieverPhone: myorder.recieverPhone,
-            order_notes: myorder.order_notes,
-            rate: myorder.rate,
-            state:"pending",
-            date: Date.now(),
-            price: myorder.price
-        }, function (err, req, res, next){
-            if (err) {
-                console.log(err);
-                //res.send("error")  
-            }
-             console.log("success")
-            // res.send("success in order")
-            });
-        }
-    })
-});
-  
+        //var id = ObjectId(JSON.parse(req.userId));
+        var id =user._id
+        var name = user.name;
+        console.log(id);
+        const myorder = req.body;
+        console.log(myorder);
+        if (!myorder)
+            return res.status(400).json({ msg: "problem in order" });
+        else if (myorder) {
+            Order.create({
+                user_id: id,
+                driver_id:"0",
+                order_details: myorder.Items,
+                location_start_lng: myorder.location_start_lng,
+                location_start_lat: myorder.location_start_lat,
+                location_end_lng: myorder.location_end_lng,
+                location_end_lat: myorder.location_end_lat,
+                reciver_name: myorder.reciverName,
+                recieverPhone: myorder.reciverPhone,
+                order_notes: myorder.order_notes,
+                name:name,
+                rate: myorder.rate,
+                state: "pending",
+                date: Date.now(),
+                price: myorder.price
+            }, function (err, req, res, next) {
+                if (err) {
+                    console.log(err);
+                    //res.send("error");
+                }
+                console.log("success",res);
+                //res.send("success in order");
+              });
+          }
+      });
+    });
   
     // User.findById((req.userId), { password: 0 }, function (err, user) {
     //     if (err)
@@ -123,7 +126,7 @@ router.get("/get_current_order", VerifyToken, function (req, res, next) {
         if (!user)
             return res.status(404).send("No user found.");
         var id = user._id;
-        Order.find({ state: "current", user_id: ObjectId(id) }).exec((err, order) => {
+        Order.find({ state: "current", user_id:id}).exec((err, order) => {
             if (err) {
                 console.log(err);
                 req.send();
@@ -140,7 +143,7 @@ router.get("/get_previous", VerifyToken, function (req, res, next) {
         if (!user)
             return res.status(404).send("No user found.");
         var id = user._id;
-        Order.find({ state: "delivered", user_id: ObjectId(id) }).exec((err, order) => {
+        Order.find({state: "delivered", user_id:id }).exec((err, order) => {
             if (err) {
                 console.log(err);
                 req.send();
