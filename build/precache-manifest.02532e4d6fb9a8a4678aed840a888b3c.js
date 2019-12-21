99487db232dafb070ce8ceb609b33f6fab53c3cf
unknown <hadil.a.hassouna@gmail.com>
2019-12-21 01:45:38 -0800

    Deploy

---
+++ build/precache-manifest.02532e4d6fb9a8a4678aed840a888b3c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e16a54d5e5be2b2935a7796cfdec40f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bef797f1c71661b9c0a",
+    "url": "/static/css/2.5ffa1744.chunk.css"
+  },
+  {
+    "revision": "176264076c3aba53baa3",
+    "url": "/static/css/main.3a9e4a03.chunk.css"
+  },
+  {
+    "revision": "4bef797f1c71661b9c0a",
+    "url": "/static/js/2.f55fe947.chunk.js"
+  },
+  {
+    "revision": "45575192a839d80087ade0f3e9384814",
+    "url": "/static/js/2.f55fe947.chunk.js.LICENSE"
+  },
+  {
+    "revision": "176264076c3aba53baa3",
+    "url": "/static/js/main.25014e6d.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "675ba7e23f3a958bc194b859f987029f",
+    "url": "/static/media/delivery.675ba7e2.jpg"
+  },
+  {
+    "revision": "002b85f296fb7d4e2dd05e2d47ebfec3",
+    "url": "/static/media/food.002b85f2.jpg"
+  },
+  {
+    "revision": "46aaf352013f9697c3330e0ae15b2856",
+    "url": "/static/media/food2.46aaf352.jpg"
+  },
+  {
+    "revision": "d1c6a72c48e43df19f557b8d1abf6c4e",
+    "url": "/static/media/gadgets.d1c6a72c.jpg"
+  },
+  {
+    "revision": "6b73399e2ac2e6a7385f8e5dc18f5fcf",
+    "url": "/static/media/logo2.6b73399e.png"
+  }
+]);
